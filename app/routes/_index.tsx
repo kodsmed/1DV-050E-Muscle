3ce2ca631ba54581552bba2b57ab3f2ec8523828
Jimmy Karlsson <jk224jv@student.lnu.se>
2024-04-03 12:05:38 +0200

    feat: add new _index.tsx to test supabase.auth

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,53 @@
+import {
+  json,
+  type LoaderFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/cloudflare";
+import { Form, useLoaderData } from "@remix-run/react";
+import { Button } from "~/components/catalyst/button";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    {
+      name: "description",
+      content: "Welcome to Remix! Using Vite and Cloudflare!",
+    },
+  ];
+};
+
+export async function loader({ context }: LoaderFunctionArgs) {
+  const user = await context.supabase.auth.getUser();
+  return json({
+    user,
+  });
+}
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+  console.log("data.user :>> ", data.user);
   return (
-    <p id="index-page">
-      This is a demo for Remix.
-      <br />
-      Check out{" "}
-      <a href="https://remix.run">the docs at remix.run</a>.
-    </p>
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
+            rel="noreferrer"
+          >
+            Cloudflare Pages Docs - Remix guide
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+      <Form action="/logout" method="post">
+        <Button type="submit">Logout</Button>
+      </Form>
+    </div>
   );
 }
