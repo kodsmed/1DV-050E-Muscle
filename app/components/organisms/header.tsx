1aa26659d165747d9f1603a9b60ff8f795671a5e
Jimmy Karlsson <jk224jv@student.lnu.se>
2024-04-03 11:55:35 +0200

    feat: add placeholder Header Component

---
+++ app/components/organisms/header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <div className="h-24 bg-slate-200 w-full flex justify-between">
+      <span>Logo here</span>
+      <nav>Nav here</nav>
+    </div>
+  );
+}
