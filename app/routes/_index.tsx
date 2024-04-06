0706548a8abe7410efc3f4376a4132cdc97a0597
Jimmy Karlsson <jk224jv@student.lnu.se>
2024-04-06 21:49:19 +0200

    feat: implement user role

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -18,14 +18,31 @@ export const meta: MetaFunction = () => {
 
 export async function loader({ context }: LoaderFunctionArgs) {
   const user = await context.supabase.auth.getUser();
+
+  // Retrieve the user's UUID and role from the database
+  const uuid = user?.data?.user?.id;
+  let role = null;
+
+  if (uuid) {
+    const { data, error } = await context.supabase
+      .rpc('getRole', { arguuid: uuid });
+    if (error) {
+      console.error(error);
+    }
+    if (data) {
+      role = data;
+    }
+  }
   return json({
     user,
+    role,
   });
 }
 
 export default function Index() {
   const data = useLoaderData<typeof loader>();
   console.log("data.user :>> ", data.user);
+  console.log("data.role :>> ", data.role);
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
@@ -45,9 +62,16 @@ export default function Index() {
           </a>
         </li>
       </ul>
-      <Form action="/logout" method="post">
+      {data.role ? (<Form action="/logout" method="post">
         <Button type="submit">Logout</Button>
-      </Form>
+      </Form>)
+        : (
+          <Form action="/login" method="get">
+            <Button type="submit">Login</Button>
+          </Form>
+        )
+      }
+
     </div>
   );
 }
