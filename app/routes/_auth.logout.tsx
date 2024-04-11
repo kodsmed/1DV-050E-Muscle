9790b395dabd7dba41c4603b8a33c5e3eb0d1d0e
Jimmy Karlsson <jk224jv@student.lnu.se>
2024-04-11 02:18:14 +0200

    fix(auth-logout): allow logout with get method

--- app/routes/_auth.logout.tsx
+++ app/routes/_auth.logout.tsx
@@ -1,6 +1,36 @@
 import { ActionFunctionArgs, redirect } from "@remix-run/cloudflare";
 import { createServerClient, parse, serialize } from "@supabase/ssr";
 
+
+export async function loader({ context, request }: ActionFunctionArgs) {
+  const cookies = parse(request.headers.get("Cookie") ?? "");
+  const headers = new Headers();
+  const supabase = createServerClient(
+    context.cloudflare.env.SUPABASE_URL!,
+    context.cloudflare.env.SUPABASE_KEY!,
+    {
+      cookies: {
+        get(key) {
+          return cookies[key];
+        },
+        set(key, value, options) {
+          headers.append("Set-Cookie", serialize(key, value, options));
+        },
+        remove(key, options) {
+          headers.append("Set-Cookie", serialize(key, "", options));
+        },
+      },
+    }
+  );
+
+  await supabase.auth.signOut();
+  const redirectTo = request.headers.get("referer") || "/";
+  return redirect(redirectTo, {
+    headers,
+  });
+}
+
+
 export async function action({ context, request }: ActionFunctionArgs) {
   const cookies = parse(request.headers.get("Cookie") ?? "");
   const headers = new Headers();
@@ -21,6 +51,7 @@ export async function action({ context, request }: ActionFunctionArgs) {
       },
     }
   );
+
   await supabase.auth.signOut();
   const redirectTo = request.headers.get("referer") || "/";
   return redirect(redirectTo, {
