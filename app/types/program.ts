3228a7867348db3d3284f022b914e38b0da5ef63
Jimmy Karlsson <jk224jv@student.lnu.se>
2024-05-03 08:41:03 +0200

    feat: add Allow users to create a training program consisting on any number of training sessions

---
+++ app/types/program.ts
@@ -0,0 +1,13 @@
+import { TRAINING_DAY_STATUS } from './enums';
+
+export interface Program {
+  id?: number;
+  created_at?: string;
+  owner_uuid?: string;
+  training_day_id: number;
+  date: string;
+  status?: TRAINING_DAY_STATUS;
+  comment?: string;
+  session_name?: string;
+}
+
