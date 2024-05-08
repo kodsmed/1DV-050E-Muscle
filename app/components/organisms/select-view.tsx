5d680a40010aa05e4d7f17d6bdaad1a076e1ed56
Jimmy Karlsson <jk224jv@student.lnu.se>
2024-05-08 22:22:32 +0200

    css: fix vertical spacing

--- app/components/organisms/select-view.tsx
+++ app/components/organisms/select-view.tsx
@@ -5,7 +5,7 @@ import { TimeFrame } from '~/types/enums';
 export function SelectView ({allMyExercises, setSelectedExercise, setSelectedTimeFrame}: {allMyExercises: Exercise[], setSelectedExercise: React.Dispatch<Exercise>, setSelectedTimeFrame: React.Dispatch<React.SetStateAction<TimeFrame>>}) {
   return (
     <div className='flex flex-row w-full justify-center h-42 border-b-2 border-slate-700 mt-8'>
-      <div className='flex- flex-row w-3/4 justify-center gap-8'>
+      <div className='flex- flex-row w-3/4 justify-center gap-8 mb-4'>
       <select onChange={(e) => setSelectedExercise(allMyExercises.find(exercise => exercise.id === parseInt(e.target.value)) as Exercise)}>
         <option value={0}>Select an exercise</option>
         {allMyExercises.map((exercise, index) => <option key={index} value={exercise.id}>{exercise.name}</option>)}
